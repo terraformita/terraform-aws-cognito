546f94e2310c19d7897ead9dab92c73694b4b9c7
Oleksandr Kukhar <kyxap@kyxap.pro>
2026-02-08 21:32:20 -0500

    chore: update lambda runtimes to latest versions (#3)

--- lambda/pre-signup/index.js
+++ lambda/pre-signup/index.js
@@ -1,10 +1,11 @@
 /*
  * Cognito Pre Sign-up Trigger: Auto-Confirm and Auto-Verify all Users
  */
-exports.handler = (event, context, callback) => {
+exports.handler = async (event) => {
+    event.response = event.response || {};
 
     // Confirm the user
-        event.response.autoConfirmUser = true;
+    event.response.autoConfirmUser = true;
 
     // Set the email as verified if it is in the request
     if (event.request.userAttributes.hasOwnProperty("email")) {
@@ -17,5 +18,5 @@ exports.handler = (event, context, callback) => {
     }
 
     // Return to Amazon Cognito
-    callback(null, event);
+    return event;
 };
